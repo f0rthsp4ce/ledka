3ac1b22168d28fbfbe9196cd21b34ce39c7c31d2
tar_xzf <tar_xzf@tuta.io>
2023-08-01 22:02:51 +0000

    Set hostname

--- main/wifi.c
+++ main/wifi.c
@@ -61,7 +61,8 @@ void wifi_start(void) {
 
   ESP_ERROR_CHECK(esp_netif_init());
   ESP_ERROR_CHECK(esp_event_loop_create_default());
-  esp_netif_create_default_wifi_sta();
+  esp_netif_t *netif = esp_netif_create_default_wifi_sta();
+  ESP_ERROR_CHECK(esp_netif_set_hostname(netif, "ledka"));
 
   s_wifi_event_group = xEventGroupCreate();
 
